8d6476a44f16d90e8d732689549f3f3789cd4b7b
John Paul Wicklund <John Paul Wicklund>
2017-02-05 15:56:41 -0600

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+// Friend Survey Results (DATA)
+// =============================================================
+var friends = [{
+  name:"Ahmed",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+    5,
+    5,
+    2,
+    4,
+    1,
+    3,
+    2,
+    5,
+    2,
+    1]
+},
+  {
+  name:"Barry",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1]
+},
+  {
+  name:"Chuck",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+    5,
+    2,
+    2,
+    5,
+    2,
+    1,
+    4,
+    4,
+    4,
+    2]
+}
+  }
+}];
--- server.js
+++ server.js
@@ -7,7 +7,13 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  // res.send("Welcome to FriendFinder!")
+  res.sendFile(path.join(__dirname, "view.html"));
+});
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -15,54 +21,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Friend Survey Results (DATA)
-// =============================================================
-var friends = [{
-  name:"Ahmed",
-  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores:[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
-}];
-
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  // res.send("Welcome to the Star Wars Page!")
-  res.sendFile(path.join(__dirname, "view.html"));
-});
 
-// Search for Specific Friend (or all friends) - provides JSON
-app.get("/api/:friends?", function(req, res) {
-  var chosen = req.params.friends;
-
-  if (chosen) {
-    console.log(chosen);
-
-    for (var i = 0; i < friends.length; i++) {
-      if (chosen === friends[i].routeName) {
-        res.json(friends[i]);
-        return;
-      }
-    }
-
-    res.json(false);
-  }
-  else {
-    res.json(friends);
-  }
-});
 
 // Create New friends - takes in JSON input
 app.post("/api/new", function(req, res) {
